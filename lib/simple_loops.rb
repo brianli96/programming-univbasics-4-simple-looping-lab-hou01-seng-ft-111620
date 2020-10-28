a76c90b52fbbaca90fa969e80594912c1c8226d1
Brian Li <brian.li96@yahoo.com>
2020-10-28 00:33:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,7 +3,7 @@ def loop_message_five_times(message)
   counter = 0
   while counter < 5 do
     puts message
-    counter + 1
+    counter += 1
   end
 end
 
@@ -11,7 +11,7 @@ def loop_message_n_times(message, number)
   counter = 0;
   while counter < number do
     puts message
-    counter + 1
+    counter += 1
   end
 end
 
@@ -19,6 +19,6 @@ def output_array(array)
   counter = 0
   while array[counter] < array.length do
     puts array[counter]
-    counter + 1
+    counter += 1
   end
 end
